7792767c4906881e372890bdfb1a4e6909e75c33
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-12-14 12:29:43 -0500

    feat: add router settings to app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routerApi = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Testing Node API');
 });
+
+routerApi(app);
 
 app.listen(port, () => {
   console.log(`Running Server on port ${port}`);
