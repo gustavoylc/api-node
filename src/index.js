b7f4ed29425f1103a47cf0af4d35bf5896a350d1
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-12-14 12:37:18 -0500

    feat: add cors middleware

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 const {
   logErrors,
   errorHandler,
   boomErrorHandler,
-  ormErrorHandler,
 } = require('./middlewares/error.handler');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
+const whitelist = ['http://localhost:8080', 'https://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not Allowed'));
+    }
+  },
+};
+app.use(cors(options));
 app.get('/', (req, res) => {
   res.send('Testing Node API');
 });
@@ -19,10 +29,7 @@ app.get('/', (req, res) => {
 routerApi(app);
 
 app.use(logErrors);
-app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Running Server on port ${port}`);
-});
+app.listen(port);
