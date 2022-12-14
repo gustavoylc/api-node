bb2f333bfe170774362051483162925a51941a7c
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-12-14 12:32:01 -0500

    feat: add middlewares settings to app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const routerApi = require('./routes');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+  ormErrorHandler,
+} = require('./middlewares/error.handler');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +17,11 @@ app.get('/', (req, res) => {
 });
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(ormErrorHandler);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Running Server on port ${port}`);
