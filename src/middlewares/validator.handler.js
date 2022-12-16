f72d3d832ce7cbe6fd028a6150a2b40558b6de8b
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-12-16 18:05:43 -0500

    feat: add validator handler

--- src/middlewares/validator.handler.js
+++ src/middlewares/validator.handler.js
@@ -2,6 +2,11 @@ const boom = require('@hapi/boom');
 
 function validatorHandler(schema, property) {
   return (req, res, next) => {
+    /**
+     * req[property] -> body -> POST
+     * req[property] -> params -> GET
+     * req[property] -> query -> QUERY
+     */
     const data = req[property];
     const { error } = schema.validate(data, { abortEarly: false });
     if (error) {
