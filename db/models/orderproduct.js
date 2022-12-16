b3f6cca9a94f2919729c40085a0996a7a1738c98
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-12-16 18:03:10 -0500

    feat: add orderproduct model

---
+++ db/models/orderproduct.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class OrderProduct extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  OrderProduct.init(
+    {
+      amount: DataTypes.INTEGER,
+      orderId: DataTypes.INTEGER,
+      productId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'OrderProduct',
+      underscored: true,
+      timestamps: true,
+    }
+  );
+  return OrderProduct;
+};
