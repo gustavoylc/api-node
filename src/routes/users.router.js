f0edb2cf4993e1c6a685b1f6f027593ce1178583
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-12-16 18:06:35 -0500

    feat: add user router

---
+++ src/routes/users.router.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const UserService = require('../services/user.service');
+const validatorHandler = require('../middlewares/validator.handler');
+const {
+  createUserSchema,
+  updateUserSchema,
+  getUserSchema,
+} = require('../schemas/user.schema');
+
+const router = express.Router();
+const service = new UserService();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await service.find();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get(
+  '/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await service.findOne(id);
+      res.json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  '/',
+  validatorHandler(createUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { body } = req;
+      const newUser = await service.create(body);
+      res.status(201).json(newUser);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.patch(
+  '/:id',
+  validatorHandler(getUserSchema, 'params'),
+  validatorHandler(updateUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { body } = req;
+      const { id } = req.params;
+      const updatedUser = await service.update(id, body);
+      res.json(updatedUser);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete(
+  '/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(204).json({ id });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+module.exports = router;
